7231df4e9f36e2b628a5fcddb9692a3008572b27
wangyafei <wyfblues@163.com>
2023-02-26 18:17:36 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const shell = require('shelljs');
+
+const path = ""
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  shell.exec(`
+  cd ${path} &&
+  git pull origin master &&
+  yarn &&
+  yarn build
+  `, function (code, stdout, stderr) {
+    console.log('Exit code:', code);
+    console.log('Program output:', stdout);
+    console.log('Program stderr:', stderr);
+    if (code === 0) {
+      console.log('成功')
+    }
+    res.send('ok');
+  });
+});
+
+module.exports = router;
